c895f476925137a91e1290f3b8159b18514587a7
unknown <C:\Users\Vlad\AppData\Roaming\The Bat!>
2025-03-25 21:40:37 +0300

    small fixes

--- recomend_api/app/api/v1/recommend.py
+++ recomend_api/app/api/v1/recommend.py
@@ -2,8 +2,6 @@ import logging
 from urllib.parse import urlencode
 
 from core.config import settings
-from core.jwt import create_access_token, verify_token
-from db.db import get_session
 from fastapi import APIRouter, Depends, HTTPException, Request, status
 from fastapi.responses import RedirectResponse
 from fastapi.security import OAuth2PasswordRequestForm
