a100192f90760eec42d5eaefc4be56b55f68d139
unknown <C:\Users\Vlad\AppData\Roaming\The Bat!>
2025-04-07 19:52:18 +0300

    branch merge

--- recommendations_api/core/utils.py
+++ recommendations_api/core/utils.py
@@ -17,13 +17,13 @@ def hash_uid(uuid_str: str) -> str:
     return str(ObjectId(hashed))
 
 
-async def is_new_user(user_id: str) -> bool:
-    """Проверяет, что у пользователя нет ни лайков, ни закладок, ни отзывов, ни просмотров."""
+# async def is_new_user(user_id: str) -> bool:
+#     """Проверяет, что у пользователя нет ни лайков, ни закладок, ни отзывов, ни просмотров."""
 
-    collections = ["likes", "bookmarks", "reviews", "watched_movies"]
+#     collections = ["likes", "bookmarks", "reviews", "watched_movies"]
 
-    for collection in collections:
-        if await db[collection].find_one({"user_id": user_id}):
-            return False  # Если хотя бы одна запись найдена, значит данные есть
+#     for collection in collections:
+#         if await db[collection].find_one({"user_id": user_id}):
+#             return False  # Если хотя бы одна запись найдена, значит данные есть
 
-    return True  # Все коллекции проверены и данные отсутствуют
+#     return True  # Все коллекции проверены и данные отсутствуют
